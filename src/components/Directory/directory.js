3b38c34c864051ce7be9e337b403055edfdce82a
Berkan Baş <brknbs@gmail.com>
2020-08-07 18:14:01 +0300

    directory content and some sass files created

---
+++ src/components/Directory/directory.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ShopMen from '../../assets/shopMens.jpg';
+import ShopWomen from '../../assets/shopWomens.jpg';
+import './directory.scss';
+import '../../sass/main.scss';
+
+const Directory = props => {
+  return (
+    <div className="directory">
+      <div className="wrap row">
+        <div className="item col-6" style={{backgroundImage: `url(${ShopWomen})`}}> 
+          <a>Shop Womens</a>
+        </div>
+        <div className="item col-6" style={{backgroundImage: `url(${ShopMen})`}}> 
+          <a>Shop Mens</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Directory;
